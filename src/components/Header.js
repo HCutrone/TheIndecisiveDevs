35e086eb1c21bbe2cdc438789413f7601c0dcdea
HCutrone <hcutrone@calpoly.edu>
2022-04-19 17:46:54 -0700

    add header

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='homeHeader'>
+      <div className='header-Setting'>Settings</div>
+      <div className='header-Name'>Novellas for the Fellas</div>
+      <div className='header-Profile'>Profile</div>
+    </div>
+  )
+}
+
+export default Header
