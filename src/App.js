9a6bb94458c16788b7e02b9b9003631f81316be7
HCutrone <hcutrone@calpoly.edu>
2022-04-19 18:37:13 -0700

    added new tabs

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import './App.css';
 import Header from './components/Header.js'
 
 function App() {
+  console.log("hello")
   return (
     <>
       <div className="App">
         <Header></Header>
+        
       </div>
     </>
   );
